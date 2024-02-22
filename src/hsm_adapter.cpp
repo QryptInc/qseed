86f4428b05865e9e61a515fbb7cce00e966bfe9a
Jay Patel <jay@qrypt.com>
2024-02-22 15:40:30 -0500

    Adding slot debugging features

--- src/hsm_adapter.cpp
+++ src/hsm_adapter.cpp
@@ -4,6 +4,9 @@
 #include <stdexcept>
 #include <dlfcn.h>
 
+const uint32_t TOKEN_LABEL_MAX_SIZE = 32;
+const uint32_t SLOT_DESCRIPTION_MAX_SIZE = 64;
+
 CryptokiAdapter::CryptokiAdapter(CryptokiConfig config) : _config(config) {
 
     // Load the PKCS#11 library dynamically
@@ -44,9 +47,49 @@ void CryptokiAdapter::injectSeedRandom(const std::vector<uint8_t>& random) {
         throw std::runtime_error(errMsg);
     }
 
+    // Get number of slots
+    CK_ULONG slotCount;
+    rv = _pFunctionList->C_GetSlotList(CK_TRUE, NULL_PTR, &slotCount);
+    if (rv != CKR_OK) {
+        std::string errMsg = "C_GetSlotList Error: " + std::to_string(rv);
+        throw std::runtime_error(errMsg);
+    }
+    printf("Number of slots with tokens: %ld\n\n", slotCount);
+    if (slotCount == 0) {
+        std::string errMsg = "No slots with tokens.";
+        throw std::runtime_error(errMsg);        
+    }
+
+    // Get slot IDs
+    CK_SLOT_ID_PTR p_slot_list = (CK_SLOT_ID_PTR) malloc(slotCount*sizeof(CK_SLOT_ID));
+    rv = _pFunctionList->C_GetSlotList(CK_TRUE, p_slot_list, &slotCount);
+    if (rv != CKR_OK) {
+        std::string errMsg = "C_GetSlotList Error: " + std::to_string(rv);
+        throw std::runtime_error(errMsg);
+    }
+
+    // Search for existing HSM token
+    for (int slotNum = 0; slotNum < slotCount; slotNum++) {
+        printf("SLOT ID: 0x%lx\n", p_slot_list[slotNum]);
+        CK_SLOT_INFO slot_info;
+        rv = _pFunctionList->C_GetSlotInfo(p_slot_list[slotNum], &slot_info);
+        std::string slotDescription((char*)slot_info.slotDescription, SLOT_DESCRIPTION_MAX_SIZE);
+        printf("SLOT DESCRIPTION: %s\n", slotDescription.c_str());
+
+        CK_TOKEN_INFO tokenInfo;
+        rv = _pFunctionList->C_GetTokenInfo(p_slot_list[slotNum], &tokenInfo);
+        if (rv == CKR_TOKEN_NOT_PRESENT) {
+            printf("NO TOKEN\n\n");
+            continue;
+        }
+        std::string token_label((char*)tokenInfo.label, TOKEN_LABEL_MAX_SIZE);
+        printf("TOKEN LABEL: %s\n\n", token_label.c_str());
+    }
+    free(p_slot_list);
+
     // Open a session
     CK_SESSION_HANDLE hSession;
-    rv = _pFunctionList->C_OpenSession(_config.slotID, CKF_SERIAL_SESSION, NULL, NULL, &hSession);
+    rv = _pFunctionList->C_OpenSession(_config.slotID, CKF_SERIAL_SESSION | CKF_RW_SESSION, NULL, NULL, &hSession);
     if (rv != CKR_OK) {
         std::string errMsg = "C_OpenSession Error: " + std::to_string(rv);
         throw std::runtime_error(errMsg);
